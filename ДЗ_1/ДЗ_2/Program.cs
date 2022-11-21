2ccf3112c5f4a95855e42ce5f443e570fe0a604a
Elena Tarasova <lena.daschkina@yandex.ru>
2022-11-21 17:17:15 +0300

    вход три числа и выдаёт максимальное из этих чисел.

---
+++ ДЗ_1/ДЗ_2/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите первое число: ");
+int num_A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int num_B = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int num_С = Convert.ToInt32(Console.ReadLine());
+
+int max=num_A;
+
+if (num_B > max)
+{
+    Console.WriteLine($"маx {max=num_B}");
+}
+if (num_С > max)
+{
+    Console.WriteLine($"маx {max=num_С}");
+}
+{
+    Console.WriteLine($"максимальное значение {max}");
+}
