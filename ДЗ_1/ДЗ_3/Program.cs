96b7df238154b5addba5da1bdd76542a0d1e76fc
Elena Tarasova <lena.daschkina@yandex.ru>
2022-11-21 18:35:17 +0300

    является ли число чётным (делится ли оно на два без остатка

---
+++ ДЗ_1/ДЗ_3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число:");
+int A = Convert.ToInt32(Console.ReadLine());
+
+if (A%2 == 1)
+{
+    Console.WriteLine($"нечетное {A}.");
+}
+else
+{
+    Console.WriteLine($"четное {A}.");
+}
