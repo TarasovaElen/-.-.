7517a8ca8b5379219d0c65128c4de892564b0ac4
Elena Tarasova <lena.daschkina@yandex.ru>
2022-11-21 16:50:47 +0300

    вывод максимального и минимального числа

---
+++ ДЗ_1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите первое число: ");
+int num_A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int num_B = Convert.ToInt32(Console.ReadLine());
+
+if (num_A > num_B)
+{
+    Console.WriteLine($"маx {num_A}, min {num_B}.");
+}
+else
+{
+    Console.WriteLine($"маx {num_B}, min {num_A}.");
+}
